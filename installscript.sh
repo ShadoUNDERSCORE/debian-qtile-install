6216d177a127a96ad46fd6d1e141a93050dc4e18
Elijah <ejamesdelhotal@gmail.com>
2025-07-22 17:29:36 -0700

    switch from code to codium

--- installscript.sh
+++ installscript.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# VSCode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+# VSCodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+  | gpg --dearmor \
+  | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg > /dev/null
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | \
-  sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main' \
+  | sudo tee /etc/apt/sources.list.d/vscodium.list
 
 # Ghostty
+curl -fsSL https://download.opensuse.org/repositories/home:clayrisser:sid/Debian_Unstable/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/trusted.gpg.d/ghostty.gpg > /dev/null
+
 echo 'deb http://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/ /' \
   | sudo tee /etc/apt/sources.list.d/ghostty.list
 
-curl -fsSL https://download.opensuse.org/repositories/home:clayrisser:sid/Debian_Unstable/Release.key \
-  | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ghostty.gpg > /dev/null
-
 # Vivaldi
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | \
-  sudo tee /etc/apt/trusted.gpg.d/vivaldi.gpg > /dev/null
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/trusted.gpg.d/vivaldi.gpg > /dev/null
 
-echo "deb [arch=amd64] https://repo.vivaldi.com/archive/deb/ stable main" | \
-  sudo tee /etc/apt/sources.list.d/vivaldi.list > /dev/null
+echo "deb [arch=amd64] https://repo.vivaldi.com/archive/deb/ stable main" \
+  | sudo tee /etc/apt/sources.list.d/vivaldi.list > /dev/null
 
 apt update
-apt install -y sudo curl qtile lightdm rofi feh picom lxappearence nemo code vivaldi-stable ghostty \
+apt install -y sudo curl qtile lightdm rofi feh picom lxappearence nemo codium vivaldi-stable ghostty \
   neovim zsh oh-my-zsh python3-pip build-essential network-manager-gnome xserver-xorg x11-utils x11-server-utils \
   papirus-icon-theme fonts-noto-color-emoji p7zip-full pipewire pipewire-audio pipewire-pulse wireplumber pavucontrol \
   bluetooth bluez blueman volumeicon
